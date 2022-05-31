9a249484f00ce12783fa3da213a823a23c4ac14b
辉煌 <2077357397@qq.com>
2022-05-31 23:02:18 +0800

    week10 commit

--- src/main/java/com/Luhuihuang/week6/demo/JDBCServletContextListener.java
+++ src/main/java/com/Luhuihuang/week6/demo/JDBCServletContextListener.java
@@ -8,34 +8,42 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//tell tomcat this class is a listener class -how?
 @WebListener
 public class JDBCServletContextListener implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent sce){
+        //like a main() method for this web app
+        //called when tomcat start
+        //use this method to create jdbc connection when tomcat start
+        ServletContext context=sce.getServletContext();
+        String driver=context.getInitParameter("driver");
+        String url=context.getInitParameter("url");
+        String username=context.getInitParameter("username");
+        String password=context.getInitParameter("password");
 
-    @Override
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
-        ServletContext context= servletContextEvent.getServletContext();
-        String driver = context.getInitParameter("driver");
-        String url = context.getInitParameter("url");
-        String username = context.getInitParameter("username");
-        String password = context.getInitParameter("password");
-
+        //now use 4 variables to get connection
         try {
             Class.forName(driver);
-            Connection con = DriverManager.getConnection(url, username, password);
-            System.out.println("init()-->" + con);
+            Connection con= DriverManager.getConnection(url,username,password);
+            System.out.println("i am contextInitialized()-- >"+con);//when?
+            //System.out.println("Connection --> in Register "+con);//just print for test
+
+            //set connection as a context attribute -- for all jsp and servlet context attribute
             context.setAttribute("con",con);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException| SQLException e) {
             e.printStackTrace();
 
         }
-        System.out.println(" i am in contextInitialized()");
+
     }
 
+
     @Override
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        System.out.println(" i am in contextDestroyed()");
-        servletContextEvent.getServletContext().removeAttribute("con");
+    public void contextDestroyed(ServletContextEvent sce) {
+        //end point of web app
+        //this method called when tomcat stop
+        System.out.println("i am in contextDestroyed()");//when this line print?
+        //remove attribute connection from context
+        sce.getServletContext().removeAttribute("con");//name
     }
 }
